78250c9328fd833df653f1a4e040ca63c094ae3e
Anuska <anuskazw@gmail.com>
2023-05-29 21:10:49 +0200

    WIP: first pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { MisProyectosComponent } from './components/mis-proyectos/mis-proyectos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentacionComponent,
+    SobreMiComponent,
+    MisProyectosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/presentacion/presentacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'presentacion',
+  templateUrl: './presentacion.component.html',
+  styleUrls: ['./presentacion.component.scss']
+})
+export class PresentacionComponent {
+
+}
